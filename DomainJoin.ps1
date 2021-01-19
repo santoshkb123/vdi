3652857a7e127392302f47ee6497dfa2cd79e156
santoshkb123 <santoshkb123@gmail.com>
2021-01-19 16:37:37 +0530

    Updated on 19th Jan 2021
    Updated on 19th Jan 2021

---
+++ DomainJoin.ps1
@@ -0,0 +1,27 @@
+﻿#Variable Section
+$ver = "v1"
+$Logfile = "C:\VMware_IT\Logs\DomainJoin-$ver.log" # Log file location
+$VDIHostname = "$env:COMPUTERNAME"
+
+Function LogWrite
+{
+	Param ([string]$logstring)
+	$datentime = Get-Date -Format g
+	$logstring = "$datentime" + ": " + "$logstring"
+	Add-content $Logfile -value $logstring
+}
+
+# Domain Join OU & Account Details
+$DOMAIN="SANTOSHLAB.COM"	# Domain Name
+$ACCOUNT="vdi_join"			# Domain account
+$PWD="P@ssw0rd123"			# Domain user Password
+$secPass=ConvertTo-SecureString $PWD -AsPlainText -Force
+$cred= New-object System.Management.Automation.PSCredential -ArgumentList “$ACCOUNT@$DOMAIN”, $secPass
+
+# OU Path
+$UATOU = "OU=VDI-UAT,OU=Workstations,DC=santoshlab,DC=com" 
+
+# Joining the Device to PreDefined OU.
+LogWrite "Computer name is $VDIHostname"
+LogWrite "Joining the Computer to the OU $UATOU"
+Add-Computer -DomainName $DOMAIN -OUPath $UATOU -credential $cred -Restart -Confirm:$false
---
+++ WS1_Staging.ps1
@@ -0,0 +1,61 @@
+﻿#Variable Section
+$scriptlogpath = "C:\VMware_IT\Logs" #local path for logs
+$version = Get-Date -UFormat %m%y%y
+$scriptfilename = "VDI_Stagging-$version.log" #local log file name
+$DestinationFolder = "C:\VMware_IT\Installs" #local destination path
+$DeviceOwnershipType = "CD" #device ownership type
+$STGSERVER = "stg.awmdm.com" #enrollment server
+$GROUPID = "VDI" #Organization group name where you want enrollment to happen
+$USERNAME = "vdistaging" #staging ccount username
+$PASSWORD = "Password@123" #staging account password
+# Download Workspace ONE® Intelligent Hub latest build
+$WS1IntelligentHub = "https://storage.googleapis.com/getwsone-com-prod/downloads/AirwatchAgent.msi"
+
+Start-Transcript $scriptlogpath\$scriptfilename
+Function WS1Downloader
+{
+Try
+{
+Start-BitsTransfer -Source $WS1IntelligentHub -Destination $DestinationFolder -ErrorAction Continue
+}
+Catch
+{
+Write-host $_.Exception
+}
+}
+# Installation Commands
+$ArgumentList = "/i C:\VMware_IT\Installs\AirwatchAgent.msi /qn /norestart ENROLL=Y IMAGE=N SERVER=$STGSERVER LGName=$GROUPID USERNAME=$USERNAME PASSWORD=$PASSWORD DEVICEOWNERSHIPTYPE=CD ASSIGNTOLOGGEDINUSER=Y"
+Function WS1Installer
+{
+Try
+{		
+ $ExitCode = (Start-Process -Filepath msiexec -ArgumentList $ArgumentList -Wait -PassThru).ExitCode
+
+    IF ($ExitCode -eq 0)
+    {
+        Write-host "WS1 Hub installed successfully..."
+        Restart-Computer -Force -Confirm:$false -ErrorAction SilentlyContinue
+    }
+    Else
+    {
+        Write-Host "failed. There was a problem installing WS1 Agent. MsiExec returned exit code $ExitCode."
+        Exit
+    }
+}
+Catch
+{
+Write-host $_.Exception
+}
+}
+
+# Verifying if the installer is present in the local drive
+IF (Test-Path -Path "C:\VMware_IT\Installs\AirwatchAgent.msi")
+{
+WS1Installer
+}
+Else
+{
+WS1Downloader
+WS1Installer
+}
+Stop-Transcript
